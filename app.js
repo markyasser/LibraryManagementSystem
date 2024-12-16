e2064adf891d558096261564bcd3b5344eaa6233
markyasser <82395903+markyasser@users.noreply.github.com>
2024-12-16 19:09:45 +0200

    rate limiter

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const sequelize = require("./config/database");
-
+const rateLimit = require("express-rate-limit");
 const booksRoutes = require("./routes/booksRoutes");
 const borrowersRoutes = require("./routes/borrowersRoutes");
 const borrowedBookRoutes = require("./routes/borrowedBookRoutes");
 
 const app = express();
+
+// Apply rate limiting to all requests
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: "Too many requests, please try again later.",
+});
+
+// Apply the rate limiting middleware globally
+app.use(limiter);
 
 app.use(bodyParser.json());
 
